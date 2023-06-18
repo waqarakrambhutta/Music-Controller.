b021a11ebdb298a59f6f7f1b7da31fec434a5203
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-06-18 14:56:26 +0500

    Adding frontend to app

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'frontend.apps.FrontendConfig',
     'api',
 ]
 
--- music_controller/urls.py
+++ music_controller/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',include('api.urls'))
+    path('home/',include('api.urls')),
+    path('',include('frontend.urls'))
 ]

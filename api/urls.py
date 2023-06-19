066a2bb547b505e4f9bb504d3b0482419a9f8ea4
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-06-19 18:03:28 +0500

    CreateRoom views.

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RoomViews
+from .views import RoomViews,CreateRoomView
 
 urlpatterns = [
-    path('api',RoomViews.as_view())
+    path('api/',RoomViews.as_view()),
+    path('api/create-room/',CreateRoomView.as_view())
 ]

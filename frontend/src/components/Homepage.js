8f174a493fe2ae689e4a529027a1ba819e669fea
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-06-21 19:59:03 +0500

    Adding a room page

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,29 +1,31 @@
-import React,{Component, createElement} from "react";
+import React, { Component } from "react";
 import Roomjoinpage from "./Roomjoinpage";
 import Createroompage from "./Createroompage";
+import Room from './Room';
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-    Redirect
-} from "react-router-dom"
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
   
 
-export default class HomePage extends Component{ 
-    constructor(props){
-        super(props);
-    }
+export default class HomePage extends Component { 
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-      return (
-        <Router>
-          <Routes>
-            <Route exact path='/' element={<p>This is the Homepage second</p>} />
-            <Route path='/join' element={<Roomjoinpage/>} />
-            <Route path='/create' element={<Createroompage />} />
-          </Routes>
-        </Router>
-      );
-    }
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path='/' element={<p>This is the Homepage</p>} />
+          <Route path='/join' element={<Roomjoinpage />} />
+          <Route path='/create' element={<Createroompage />} />
+          <Route path='/room/:id' Component={Room} />
+        </Routes>
+      </Router>
+    );
+  }
 }
